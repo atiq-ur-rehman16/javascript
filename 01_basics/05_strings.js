3013d21e55a739c4047b206a513603f47ddb9e67
Atiq Ur Rehman <atiqurrehman4116@gmail.com>
2025-12-18 18:16:46 +0500

    Strings2

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -3,16 +3,35 @@ const repoCount = 50 ;
 
 // console.log(name + repoCount + " Value" );
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 
-const gameName = new String('atiq')
+const gameName = new String('atiq-ur')
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
 
 
 
-console.log(gameName.length);
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+// console.log(gameName.length);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+const anotherString = gameName.slice(-6, 4);
+console.log(anotherString);
+
+const newStringOne = "    atiq ur rahman     ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "www.atiqurrahman.com/about%20me";
+
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('about'));
+
+console.log(gameName.split('-'));
